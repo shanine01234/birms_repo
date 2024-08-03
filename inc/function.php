3aa9fc9b63985f1456dade3b3007213892569ca1
shanine01234 <shanine01234@gmail.com>
2024-08-03 19:09:32 +0800

    commit

--- inc/function.php
+++ inc/function.php
@@ -4,9 +4,9 @@
     // DATABASE CONNECTION
     class Connection{
         public $host = "localhost";
-        public $user = "u510162695_db";
+        public $user = "u510162695_birms_db";
         public $password = "1Birms_db";
-        public $db_name = "u510162695_db";
+        public $db_name = "u510162695_birms_db";
         public $conn;
 
 
